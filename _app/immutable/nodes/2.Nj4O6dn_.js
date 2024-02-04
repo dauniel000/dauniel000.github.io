e974f415daee0bc494759082c57daace4c8f4f4b
Daniel <152334453+dauniel000@users.noreply.github.com>
2024-02-04 13:03:39 +0100

    Delete _app directory

--- _app/immutable/nodes/2.Nj4O6dn_.js
+++
@@ -1,9 +0,0 @@
-import{s as b,n as m}from"../chunks/scheduler.zMJaRgub.js";import{S as y,i as g,e as p,H as v,a as r,s as w,h as H,c as h,b as T,d as o,f as M,g as q,j as c,k as u,l as f}from"../chunks/index.5X1gVZ24.js";function C(x){let t,n,_=`<script type="application/ld+json">
-        {
-            "@context": "http://blem.fun/",
-            "@type": "dauniel000",
-                "name": "blem",
-                "description": "Very blem site"
-                "url": "/"
-        }
-        <\/script>`,l,i,e,d='<h1 class="text-7xl text-blue-300 ">console.log(<span class="font-mono">&quot;Hello world!&quot;</span>)</h1> <p class="mt-10 text-3xl">My name is <span class="font-bold font-mono">dauniel000</span></p> <p class="mt-8 text-xl">This is the <span class="font-mono font-bold">temporary</span> website. If i got some time <span class="font-mono font-bold">i build better one</span>!</p> <h2 class="text-4xl mt-5">Contact:</h2> <ol class="text-2xl mt-2"><li>discord: <span class="font-bold font-mono">blemwom</span></li></ol>';return{c(){t=p("meta"),n=new v(!1),l=r(),i=w(),e=p("div"),e.innerHTML=d,this.h()},l(s){const a=H("svelte-2ylxix",document.head);t=h(a,"META",{name:!0,content:!0}),n=T(a,!1),l=r(),a.forEach(o),i=M(s),e=h(s,"DIV",{class:!0,"data-svelte-h":!0}),q(e)!=="svelte-13153pl"&&(e.innerHTML=d),this.h()},h(){document.title="dauniel000 | site building!",c(t,"name","description"),c(t,"content","Site is building for now!"),n.a=l,c(e,"class","px-32 py-10")},m(s,a){u(document.head,t),n.m(_,document.head),u(document.head,l),f(s,i,a),f(s,e,a)},p:m,i:m,o:m,d(s){s&&(n.d(),o(i),o(e)),o(t),o(l)}}}class E extends y{constructor(t){super(),g(this,t,null,C,b,{})}}export{E as component};
